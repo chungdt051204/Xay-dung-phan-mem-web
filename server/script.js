4d649311a8acda20ed471de3fa77bab32b35d412
chungdt051204 <email_github_ca_bn@gmail.com>
2026-01-31 09:40:07 +0700

    Bổ sung kết nối database với mongoose

--- server/script.js
+++ server/script.js
@@ -1,6 +1,10 @@
+const connectDB = require("./database");
+connectDB();
 const express = require("express");
 const app = express();
 const port = 3000;
+const categoryRouter = require("./modules/category/category.router");
+app.use("/", categoryRouter);
 app.get("/", (req, res) => {
   return res.json("Hello world");
 });
